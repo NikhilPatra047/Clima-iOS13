7650b4b5f611e1e905aedf5c5107836d25da19cb
Nikhil Kumar Patra <56184525+NikhilPatra047@users.noreply.github.com>
2022-04-10 11:03:01 +0530

    Added the UITextFieldDelegate protocol to the viewController, and added the some functionality to the app.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,43 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchFieldText: UITextField!
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchFieldText.endEditing(true)
+        print(searchFieldText.text!)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchFieldText.delegate = self
     }
 
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchFieldText.endEditing(true)
+        print(searchFieldText.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+     }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchFieldText.text = ""
+        searchFieldText.placeholder = "Search"
+    }
 }
 
